135548e308c887cefd30402822a61c271aaa472e
Bobby De Luna <bobby.deluna2@gmail.com>
2023-05-01 22:24:26 -0700

    Completed Assignment

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username:"Blam",password_digest:"Blam",image_url:"What",bio:"Blam")
+puts "Done Seeding"
